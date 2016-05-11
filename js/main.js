f5a21d42cec5dba0ad23e50916f858a4c4bc273a
Yeondam Park <giyen9088@gmail.com>
2016-05-10 23:11:46 -0700

    Modified Sass
    Add & Remove unnecessary things

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.name').typeIt({
+	    strings: ["Hello World!", "I am Yeondam!"],
+	    startDelay: 300,
+	    speed: 100,
+	    breakLines: false,
+	    loop: true,
+	    loopDelay: 750
+	});
+});
+
